2cc1560a72c5d0487003f99776c187bd72e83977
Ty-Zeta <tyofzeta@gmail.com>
2017-09-04 20:50:53 -0400

    have basic code working but value's not passing through .scan

--- app.rb
+++ app.rb
@@ -119,3 +119,15 @@ end
 get '/isbn' do
     erb :isbn
 end
+
+post '/isbn' do
+    session[:ten_isbn] = params[:ten_digit_box]
+    session[:thirteen_isbn] = params[:thirteen_digit_box]
+    isbn_truth = check_10_sum(:ten_isbn)
+    isbn_truth = check_13_sum(:thirteen_isbn) 
+    redirect '/isbn_results'
+end
+
+get 'isbn_results' do
+    erb :isbn_results, locals: {ten_result: session[:ten_isbn], thirteen_result: session[:thirteen_isbn]}
+end
--- isbn.rb
+++ isbn.rb
@@ -1,17 +1,3 @@
-def empty_array
-    []
-end
-
-def isbn_length(isbn)
-    if isbn.length == 10
-        true
-    elsif isbn.length == 13
-        true
-    else
-        false
-    end
-end 
-
 def isbn_dashx(isbn)
     clean_isbn = isbn.delete(" -")
 
@@ -23,18 +9,18 @@ def isbn_dashx(isbn)
     end
 end
 
-def check_sum(isbn)
+def check_10_sum(ten_digit_box)
     sum = 0
 
-    clean_isbn = isbn.scan /\w/
+    clean_isbn = ten_digit_box.scan /\w/
     last_digit = clean_isbn.pop
     
     if last_digit == "x"
-            last_digit = last_digit.to_s
+        last_digit = last_digit.to_s
     end
     
         clean_isbn.each_with_index do |value, index_position|
-        sum = sum + ((index_position + 1).to_i * value.to_i)
+        sum += ((index_position + 1).to_i * value.to_i)
     end
 
     mod = sum % 11
@@ -43,6 +29,30 @@ def check_sum(isbn)
             
         elsif last_digit.to_i == mod.to_i
             true
+            isbn_true = true
+        else
+            false
+        end
+end
+
+def check_13_sum(thirteen_digit_box)
+    sum = 0
+
+    clean_isbn = thirteen_digit_box.scan /\w/
+    last_digit = clean_isbn.pop
+
+    clean_isbn.each_with_index do |value, index_position|
+        if index_position.to_i % 2 == 0
+          sum += value.to_i * 1
+        else
+          sum += value.to_i * 3
+        end
+    end
+
+    mod = (10 - (sum % 10)) % 10
+        if mod == last_digit.to_i
+            true
+            isbn_true = true
         else
             false
         end
