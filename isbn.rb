0617c3c6d6f0705212ef887eb2bffe856a713da9
Ty Cook <tycook@Tys-MacBook-Pro.local>
2017-09-15 16:20:57 -0400

    please work

--- isbn.rb
+++ isbn.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'aws-sdk'
 require 'csv'
-load "./local_env.rb" 
+load './local_env.rb' if File.exist?('./local_env.rb')
 
 def isbn_function(user_given_isbn)
     no_space_or_dash_isbn = user_given_isbn.delete(' -')
